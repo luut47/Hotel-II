49b2619a19203034bc3d8951e0136beec751a036
luut47 <luuminhthang2807@gmail.com>
2026-04-16 00:03:35 +0700

    login / request to server

--- app/src/main/java/com/example/hotellapp/MainActivity.java
+++ app/src/main/java/com/example/hotellapp/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.hotellapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.hotellapp.activities.LoginActivity;
+import com.example.hotellapp.activities.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnGoLogin, btnGoRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnGoLogin = findViewById(R.id.btnGoLogin);
+        btnGoRegister = findViewById(R.id.btnGoRegister);
+
+        btnGoLogin.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, LoginActivity.class))
+        );
+
+        btnGoRegister.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class))
+        );
     }
 }
--- app/src/main/java/com/example/hotellapp/activities/SplashActivity.java
+++ app/src/main/java/com/example/hotellapp/activities/SplashActivity.java
@@ -4,23 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.hotellapp.utils.SessionManager;
 import com.example.hotellapp.utils.RoleRouter;
+import com.example.hotellapp.utils.SessionManager;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedIntancesState){
-        super.onCreate(savedIntancesState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         SessionManager sessionManager = new SessionManager(this);
-        if(sessionManager.isLoggedIn() && sessionManager.getToken() != null){
+
+        if (sessionManager.isLoggedIn() && sessionManager.getToken() != null) {
             RoleRouter.goToHomeByRole(this, sessionManager.getRole());
-        }else{
-            startActivities(new android.content.Intent(this, LoginActivity.class));
+        } else {
+            startActivity(new Intent(this, LoginActivity.class));
             finish();
         }
-    }
-
-    private void startActivities(Intent intent) {
-
     }
 }
